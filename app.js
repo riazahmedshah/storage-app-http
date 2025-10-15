dc656dcd29f729f5bbd21a83e61ddd52b55a553a
riazahmedshah <riyazsh360@gmail.com>
2025-10-16 00:17:40 +0530

    open/download feature

--- app.js
+++ app.js
@@ -2,12 +2,14 @@ import http from "node:http";
 import { readdir, open, readFile } from "node:fs/promises";
 
 const serveFiles = async (req, res) => {
-  const files = await readdir(`./storage${decodeURIComponent(req.url)}`);
+  const [url, params] = req.url.split("?");
+  // console.log("PATH",req.url,decodeURIComponent(url));
+  const files = await readdir(`./storage${decodeURIComponent(url)}`);
   let dynamicHTML = ``;
   for (const file of files) {
-    dynamicHTML += `<li><a target="_blank" href="${req.url === "/" ? "" : req.url}/${file}">
-      ${file}
-    </a></li>`;
+    dynamicHTML += `${file} <a href="${req.url === "/" ? "" : req.url}/${file}?action=open">open</a> <a href="${req.url === "/" ? "" : req.url}/${file}?action=download">
+        Download
+      </a> </br>`;
   }
   res.writeHead(200, { 'Content-Type': 'text/html' });
   const boilerPlateHTML = await readFile("boilerPlate.html", "utf-8");
@@ -21,13 +23,25 @@ const server = http.createServer(async (req, res) => {
     serveFiles(req,res);
   } else {
     try {
-      const fileHandle = await open(`./storage${decodeURIComponent(req.url)}`);
+      const [url, queryStrings] = req.url.split("?");
+      
+      const queryParams = {};
+      queryStrings.split("&").forEach((pair) => {
+        const [key, value] = pair.split("=");
+        queryParams[key] = value;
+      })
+      const fileHandle = await open(`./storage${decodeURIComponent(url)}`);
       const stat = await fileHandle.stat();
       if (stat.isDirectory()) {
         serveFiles(req,res);
       } else{
-        const readStream = fileHandle.createReadStream();
-        readStream.pipe(res);
+          res.setHeader('Content-Disposition', 'attachment')
+          const readStream = fileHandle.createReadStream();
+          if(queryParams.action === "open"){
+            
+          }
+          readStream.pipe(res);
+        
       }
     } catch (error) {
       console.log(error.message);
